7530dbc6ef8f94cade29b1c69848d70014d1324a
sergioasn <sergioasn@sergioasn>
2020-04-11 16:51:25 +0200

    change rtl

--- src/adder.v
+++ src/adder.v
@@ -12,32 +12,26 @@ module adder(
   input dv,
   input [31:0]   data0_in,
   input [31:0]   data1_in,
-  output  [31:0] data_out,
-  output  dv_out);
-
-reg [31:0] r_sum_out;
-reg  r_dv_out;
+  output reg [31:0] data_out,
+  output reg dv_out);
 
 always @ (posedge clk) begin
   if( reset) begin
-    r_sum_out <= 0;
-    r_dv_out  <= 0;
+    data_out <= 0;
+    dv_out  <= 0;
   end
   else begin
-  r_dv_out  <= 0;
+  dv_out  <= 0;
     if( dv == 1) begin
-      r_sum_out <= data0_in + data1_in;
-      r_dv_out  <= 1;
+      data_out <= data0_in + data1_in;
+      dv_out  <= 1;
     end
-    // else begin
-    // end
   end
 end
 
-assign data_out = r_sum_out;
-assign dv_out = r_dv_out;
 `ifndef VERILATOR
   // Dump waves
+  // Gtkwave file
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars(1, adder);
